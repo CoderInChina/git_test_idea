392546536d7a80257a8b2110958e6e1a3a0a0d1e
xiaoming <xiaoming@qq.com>
2021-05-12 10:44:12 +0800

    张三再次添加了aa方法

--- 01-git代码的测试/src/com/gec/aa/DemoTest1.java
+++ 01-git代码的测试/src/com/gec/aa/DemoTest1.java
@@ -1,6 +1,12 @@
 package com.gec.aa;
 
 public class DemoTest1 {
+
+    public static void aa()
+    {
+        System.out.println("aaa");
+    }
+
     public static void main(String[] args) {
 
 
@@ -17,13 +23,17 @@ public class DemoTest1 {
         System.out.println("111111111111111111111");
 
 
-        System.out.println("---------------------------");
+        System.out.println("--------------1111-------------");
 
 
        // void aa();
 
 
         System.out.println("-----------sajskajskajskaj");
+
+
+
+
 
 
     }
